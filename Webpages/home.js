b3f5eb11e71d06daf3575aab931a5ccb4e5f8c5e
raghav0101 <raghavraghuwanshi0101@gmaqil.com>
2019-10-25 15:47:44 +0530

    prog

--- Webpages/home.js
+++ Webpages/home.js
@@ -1,3 +1,31 @@
+  //Already present script
+
+  // Get the Sidebar
+  var mySidebar = document.getElementById("mySidebar");
+
+  // Get the DIV with overlay effect
+  var overlayBg = document.getElementById("myOverlay");
+  
+  // Toggle between showing and hiding the sidebar, and add overlay effect
+  function w3_open() {
+    if (mySidebar.style.display === 'block') {
+      mySidebar.style.display = 'none';
+      overlayBg.style.display = "none";
+    } else {
+      mySidebar.style.display = 'block';
+      overlayBg.style.display = "block";
+    }
+  }
+  
+  // Close the sidebar with the close button
+  function w3_close() {
+    mySidebar.style.display = "none";
+    overlayBg.style.display = "none";
+  }
+  
+  //Already present script over
+
+//Open the file complaint box
 function fileComplaint()
 {
 
@@ -5,6 +33,7 @@ function fileComplaint()
     try{
     var form = document.getElementById('contact');//.style.display('block');
     form.style.display = 'block';
+    
 }
     catch(error)
     {
@@ -33,76 +62,174 @@ function openLink(evt, linkName) {
 
 
   //Dropdown menu
-
-  function myDropDownFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-function closeDropdown(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        
-      }
-    }
-  }
-}
-
- 
-window.onclick = function(event)
+function whatChecked(that,othName)
 {
-  this.closeDropdown(event);
-}
+  if(that.value=='other')
+  document.getElementsByName(othName)[0].style.visibility = 'visible';
 
-
-function selectFromDropDown(num)
-{
-  const types = ['electrical','water','network','other'];
-  var disp = document.getElementById("DropdownDisplay")
-
-  disp.innerHTML = types[num];
-
-  const oth = document.getElementsByName('HostelOther');
-  if(num==3)
-  oth[0].style.visibility = 'visible';
   else
-  oth[0].style.visibility = 'hidden';
+  document.getElementsByName(othName)[0].style.visibility = 'collapse';
 }
-
-
 //Dropdown Over
 
+//Submit Complaint
 
 
-
-  //Already present script
-
-  // Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
-
-// Get the DIV with overlay effect
-var overlayBg = document.getElementById("myOverlay");
-
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-  if (mySidebar.style.display === 'block') {
-    mySidebar.style.display = 'none';
-    overlayBg.style.display = "none";
-  } else {
-    mySidebar.style.display = 'block';
-    overlayBg.style.display = "block";
+function compSubmit(type)
+{
+  switch(type)
+  {
+    case 'hostel' :
+    submitHostel();break;
+    case 'fees' :
+    submitFees();break;
+    case 'mess' :
+    submitMess();break;
+    case 'fac' :
+    submitFac();break;
+    case 'lab' :
+    submitLab();break;
+    case 'library' :
+    submitLibrary();break;
   }
-}
 
-// Close the sidebar with the close button
-function w3_close() {
-  mySidebar.style.display = "none";
-  overlayBg.style.display = "none";
-}
+  function submitHostel()
+  {
+    
+    const category = document.querySelector('input[name="hostelCat"]:checked').value;
+    
+    const relatedTo = document.getElementById('hostelSelect').value;
 
-//Already present script over
+    var otherRel = "";
+
+    if(relatedTo== 'other')
+    otherRel = ":"+document.getElementsByName('hostelOther')[0].value;
+
+    const description = document.getElementById('hostelDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      type : relatedTo+otherRel,
+                      desc : description
+                    }
+
+    alert(toBeSent.category+" "+toBeSent.Htype +" "+toBeSent.desc);
+  }
+
+  function submitFees()
+  {
+    
+    const category = document.querySelector('input[name="feesCat"]:checked').value;
+    
+
+    const description = document.getElementById('feesDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      desc : description
+                    }
+
+    alert(toBeSent.category+" "+toBeSent.desc);
+  }
+
+
+  function submitMess()
+  {
+    
+    const category = document.querySelector('input[name="messCat"]:checked').value;
+    
+    const relatedTo = document.getElementById('messSelect').value;
+
+    var otherRel = "";
+    if(relatedTo== 'other')
+    otherRel = ":"+document.getElementsByName('messOther')[0].value;
+
+    const description = document.getElementById('messDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      type : relatedTo+otherRel,
+                      desc : description
+                    }
+
+    alert(toBeSent.category+" "+toBeSent.type +" "+toBeSent.desc);
+  }
+
+  function submitFac()
+  {
+    
+    const category = document.querySelector('input[name="facCat"]:checked').value;
+    
+    const dept = document.getElementById('facSelect').value;
+    
+    const name = document.getElementsByName('facName')[0].value;
+    
+    const description = document.getElementById('facDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      dept : dept,
+                      name : name,
+                      desc : description
+                     }
+
+    
+    alert(toBeSent.category+" "+toBeSent.dept +" "+toBeSent.name+" "+toBeSent.desc);
+  }
+
+  function submitLab()
+  {
+    
+    const category = document.querySelector('input[name="labCat"]:checked').value;
+    
+    const dept = document.getElementById('labSelect').value;
+
+    const name = document.getElementsByName('labName')[0].value;
+   
+    const description = document.getElementById('labDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      dept: dept,
+                      name : name,
+                      desc : description
+                    }
+
+    alert(toBeSent.category+" "+toBeSent.dept +" "+toBeSent.name+" "+toBeSent.desc);
+  }
+
+  function submitLibrary()
+  {
+    
+    const category = document.querySelector('input[name="libCat"]:checked').value;
+    
+    const relatedTo = document.getElementById('libSelect').value;
+
+    var otherRel = "";
+
+    if(relatedTo== 'other')
+    otherRel = ":"+document.getElementsByName('libOther')[0].value;
+
+    const description = document.getElementById('libDesc').value;
+
+
+    const toBeSent = {
+                      category: category,
+                      type : relatedTo+otherRel,
+                      desc : description
+                    }
+
+    alert(toBeSent.category+" "+toBeSent.type +" "+toBeSent.desc);
+  }
+  
+
+
+
+}
+//Submit Complaint Over
+
